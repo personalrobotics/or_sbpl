06b21ad1c846e04c7d2a435d9f6a4b9931855276
Aaron Walsman <awalsman@andrew.cmu.edu>
2014-05-06 14:24:58 +0000

    Adding YAML parsing

--- src/CachedAction.cpp
+++ src/CachedAction.cpp
@@ -3,11 +3,12 @@
 
 using namespace or_sbpl;
 
+CachedAction::CachedAction(){}
+
 CachedAction::CachedAction(const std::vector<WorldCoordinate> &pts) :
     _pts(pts) {
     setName("cached");
 }
-
 
 bool CachedAction::apply(const WorldCoordinate &wc, const OpenRAVE::RobotBasePtr &robot, WorldCoordinate &final_wc) const {
     
@@ -59,4 +60,62 @@ std::vector<WorldCoordinate> CachedAction::applyWithIntermediates(const WorldCoo
     rStateSaver.Restore();
 
     return intermediates;
+}
+
+void operator >> (const YAML::Node& node,
+                  boost::unordered_map<int,
+                  std::vector<CachedAction> >& actions){
+    actions.clear();
+    for(YAML::Iterator field = node.begin(); field != node.end(); ++field){
+        std::string key;
+        field.first() >> key;
+        const YAML::Node& value = field.second();
+        if(key == "actions"){
+            for(YAML::Iterator it = value.begin(); it != value.end(); ++it){
+                int angle;
+                std::vector<CachedAction> angle_actions;
+                for(YAML::Iterator ii = (*it).begin(); ii != (*it).end(); ++ii){
+                    std::string action_key;
+                    ii.first() >> action_key;
+                    const YAML::Node& action_value = ii.second();
+                    if(action_key == "angle"){
+                        action_value >> angle;
+                    }
+                    else if(action_key == "poses"){
+                        action_value >> angle_actions;
+                    }
+                }
+                actions[angle] = angle_actions;
+            }
+        }
+    }
+}
+
+void operator >> (const YAML::Node& node, std::vector<CachedAction>& actions){
+    actions.resize(node.size());
+    size_t ii = 0;
+    for(YAML::Iterator it = node.begin(); it != node.end(); ++it, ++ii){
+        *it >> actions[ii];
+        //std::cout << actions[ii]._pts[1].x << " " << actions[ii]._pts[1].y << std::endl;
+    }
+}
+
+void operator >> (const YAML::Node& node, CachedAction& action){
+    std::vector<WorldCoordinate> pts;
+    pts.clear();
+    
+    for(YAML::Iterator it = node.begin(); it != node.end(); ++it){
+        YAML::Iterator xyz = it->begin();
+        WorldCoordinate action_coord;
+        (*xyz) >> action_coord.x;
+        ++xyz;
+        (*xyz) >> action_coord.y;
+        ++xyz;
+        (*xyz) >> action_coord.theta;
+        pts.push_back(action_coord);
+        //std::cout << action_coord.x << " " << action_coord.y << " " << action_coord.theta << std::endl;
+    }
+    //std::cout << "A" << pts[3].x << " " << pts[3].y << " " << pts[3].theta << std::endl;
+    action = CachedAction(pts);
+    //std::cout << action._pts[3].x << " " << action._pts[3].y << " " << action._pts[3].theta << std::endl;
 }
