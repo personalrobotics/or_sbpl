b4cc08b288892feeef9685a481cccd3a3dedeeee
Michael Koval <mkoval@cs.cmu.edu>
2015-01-28 18:30:30 -0500

    Switched to using openrave_catkin.

--- catkin.cmake
+++ catkin.cmake
@@ -1,27 +1,25 @@
 cmake_minimum_required(VERSION 2.8.3)
 project(or_sbpl)
 
-# TODO: We might be missing dependencies here.
-find_package(catkin REQUIRED)
+find_package(catkin REQUIRED COMPONENTS openrave_catkin)
 catkin_package(
     INCLUDE_DIRS include/
     LIBRARIES ${PROJECT_NAME}
 )
 
+find_package(OpenRAVE REQUIRED)
+
 include(FindPkgConfig)
-pkg_check_modules(OPENRAVE REQUIRED openrave0.9)
 pkg_check_modules(SBPL REQUIRED sbpl)
 pkg_check_modules(Yaml REQUIRED yaml-cpp)
 
-MESSAGE(STATUS "openrave0.0: " ${OPENRAVE_INCLUDE_DIRS})
-
 include_directories(
   include
-  ${OPENRAVE_INCLUDE_DIRS}
+  ${OpenRAVE_INCLUDE_DIRS}
   ${SBPL_INCLUDE_DIRS}
   ${Yaml_INCLUDE_DIRS})
 
-link_directories(${OPENRAVE_LIBRARY_DIRS} ${SBPL_LIBRARY_DIRS} ${Yaml_LIBRARY_DIRS})
+link_directories(${OpenRAVE_LIBRARY_DIRS} ${SBPL_LIBRARY_DIRS} ${Yaml_LIBRARY_DIRS})
 
 add_library(${PROJECT_NAME} 
   src/SBPLBasePlanner.cpp 
@@ -30,17 +28,14 @@ add_library(${PROJECT_NAME}
   src/CachedAction.cpp
   src/TwistAction.cpp)
 target_link_libraries(${PROJECT_NAME}
-  ${SBPL_LIBRARIES} ${OPENRAVE_LIBRARIES} ${Yaml_LIBRARIES})
+    ${SBPL_LIBRARIES} ${OpenRAVE_LIBRARIES} ${Yaml_LIBRARIES})
 
-add_library(${PROJECT_NAME}_plugin src/SBPLMain.cpp)
+openrave_plugin(${PROJECT_NAME}_plugin src/SBPLMain.cpp)
 target_link_libraries(${PROJECT_NAME}_plugin
-  ${SBPL_LIBRARIES} ${OPENRAVE_LIBRARIES})
-set_target_properties(${PROJECT_NAME}_plugin PROPERTIES PREFIX "")
+    ${SBPL_LIBRARIES} ${OpenRAVE_LIBRARIES})
 
 install(TARGETS or_sbpl
     LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION})
-install(TARGETS or_sbpl_plugin
-    LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}/openrave0.9)
 install(DIRECTORY include/${PROJECT_NAME}/
     DESTINATION ${CATKIN_PACKAGE_INCLUDE_DESTINATION}
     PATTERN ".svn" EXCLUDE)
